a2880c0735cc5c75814fb705b24832e6f1230e2d
Justin <jgwitkowski@gmail.com>
2014-11-26 21:33:08 -0500

    Initial Styling and more content with working nav

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,26 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/group-lessons', {
+        templateUrl: 'views/group-lessons.html',
+        controller: 'GroupLessonsCtrl'
+      })   
+      .when('/indoor-golf', {
+        templateUrl: 'views/indoor-golf.html'
+        // controller: 'IndoorGolfCtrl'
+      }) 
+      .when('/junior-clinics', {
+        templateUrl: 'views/junior-clinics.html'
+        // controller: 'JuniorClinicsCtrl'
+      })     
+      .when('/private-lessons', {
+        templateUrl: 'views/private-lessons.html'
+        // controller: 'JuniorClinicsCtrl'
+      }) 
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+        // controller: 'JuniorClinicsCtrl'
+      })                              
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/group-lessons.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fennergolfApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fennergolfApp
+ */
+angular.module('fennergolfApp')
+  .controller('GroupLessonsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
